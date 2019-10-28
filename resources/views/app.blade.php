b8be6f0c726f17e5d36dbebfc4a38125871e38ea
shuttlesaifdev <saiful@shuttlebd.com>
2019-10-28 15:44:49 +0600

    layout building

---
+++ resources/views/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" type="text/css" href="css/app.css">
+    </head>
+    <body class="font-sans">
+       <div id="app">
+            <div class="container mx-auto">
+                <header class="py-6 mb-8">
+                    <h1>
+                        <img alt="Laracasts" src="/images/logo.svg">
+                    </h1>
+                </header>
+
+                <main class="flex">
+                    <aside class="w-1/5">
+                        <section class="mb-8">
+                            <h5 class="uppercase font-bold mb-3">The Brand</h5>
+
+                            <ul class="list-reset">
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/">Logo</router-link></li>
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Logo Symbol</router-link></li>
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Colors</router-link></li>
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Typography</router-link></li>
+                            </ul>
+                        </section>
+
+                        <section>
+                            <h5 class="uppercase font-bold mb-3">Doodles</h5>
+
+                            <ul class="list-reset">
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/">Mascot</router-link></li>
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Illustrations</router-link></li>
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/">Loaders and Animations</router-link></li>
+                               <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Wallpapers</router-link></li>
+                            </ul>
+                        </section>
+                    </aside>
+
+                    <div class="primary flex-1">
+                       <router-view></router-view>
+                    </div>
+                </main>
+            </div>
+       </div>
+       <script src="/js/app.js"></script>
+    </body>
+</html>
